4669ac449873c4ed1199a7b6ef43219e50e1ead3
mangonreg <mangonreg@alum.us.es>
2021-03-13 15:38:13 +0000

    web server

--- index.js
+++ index.js
@@ -1,4 +1,44 @@
 //Cuando inicializamos un paquete debemos crear una variable y usar la función require("nombre de la carpeta contenedora del modulo");
+
 var cool = require("cool-ascii-faces");
 
-console.log(cool);
+/*Para el desarrollo de webs sencillas usaremos el modulo express. Al igual que 
+el anterior, debemos incluirlo en una variable con la función require. Express aporta
+una función constructora
+*/
+
+var express = require("express");
+
+var app = express();
+
+//Definimos los puertos
+
+var port = 10000;
+
+//Definimos la fecha para conocer cuando se realizan las peticiones
+
+var date = new Date();
+
+//Definimos las rutas
+
+app.get("/cool", (request,response) => {
+	//La respuesta que se enviará a la peticion /cool sera una carita de cool-ascii-faces
+	response.send(cool());
+	//Sacamos por pantalla que se ha recibido la petición
+	var fecha = date.getDate() + "/" + (date.getMonth()+1) + "/" + date.getFullYear();
+	var hora = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+	console.log("New Request " + fecha + " " + hora);
+});
+
+//Indicamos que hará el servidor a la escucha
+
+app.listen(port,() => {
+	console.log("Server is listening on port " + port);
+});
+
+/*
+//Se debe llamar como si se tratase de una función
+console.log(cool());
+*/
+
+//El comando npm install instala todos los paquetes que necesita el proyecto si se clona desde git
