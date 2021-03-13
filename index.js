41ea416987d67d0c249bad99626d0f5ed8368d65
mangonreg <mangonreg@alum.us.es>
2021-03-13 14:55:13 +0000

    primer commit

---
+++ index.js
@@ -0,0 +1,4 @@
+//Cuando inicializamos un paquete debemos crear una variable y usar la función require("nombre de la carpeta contenedora del modulo");
+var cool = require("cool-ascii-faces");
+
+console.log(cool);
