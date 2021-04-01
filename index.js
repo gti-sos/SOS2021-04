5b66b113d7899739a2d8d0d4d9e8391836101324
migueclon98 <40550311+migueclon98@users.noreply.github.com>
2021-04-01 13:59:41 +0200

    asegurarse de que no hay repetidos datos iniciales

--- index.js
+++ index.js
@@ -258,7 +258,7 @@ var BASE_API_PATH = "/api/v1";
 		// Incluimos los datos en el array 
 
 		for(var e in datosIniciales_EE){
-			illiteracy_array.pop(datosIniciales_EE[e]);
+			//illiteracy_array.pop(datosIniciales_EE[e]);
 			if(datosIniciales_EE[e] in illiteracy_array){
 				
 			}
