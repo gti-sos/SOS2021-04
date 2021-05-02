1c949ae294961f881ddc83ee615fd87f86e6152f
migueclon98 <40550311+migueclon98@users.noreply.github.com>
2021-05-03 01:01:36 +0200

    app y edit prueba1

--- src/App.svelte
+++ src/App.svelte
@@ -5,13 +5,19 @@
 	import Home from "./pages/Home.svelte";
 	import Info from "./pages/Info.svelte";
 	import Education_Expenditures from "./front/education_expenditures/App.svelte";
+	import illiteracy from "./front/illiteracy/illiteracyApp.svelte";
 	
 	const routes = {
 		"/":Home,
 		"/info":Info,
 		'*': NotFound,
-		"/education_expenditures":Education_Expenditures
+		"/education_expenditures":Education_Expenditures,
+		"/illiteracy":illiteracy
 		};
+
+
+
+
 </script>
 
 <main>
